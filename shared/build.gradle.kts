26e42bc1d74dbb7c5fc055385d085ac4278afd48
kgetman <kgetmanskii@gmail.com>
2026-07-06 15:08:04 +0200

    feat(AB BLE connection > prerequisites): add kable, kotlinx deps

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -47,6 +47,8 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.kable.core)
+            implementation(libs.kotlinx.coroutines.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)

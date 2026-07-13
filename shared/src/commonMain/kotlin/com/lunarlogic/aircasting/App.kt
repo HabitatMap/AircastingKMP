83f8f0af5ea841bc11df304bb296559b876a8496
kgetman <kgetmanskii@gmail.com>
2026-07-13 13:04:33 +0200

    feat(AB BLE connection > prerequisites): add log device size

--- shared/src/commonMain/kotlin/com/lunarlogic/aircasting/App.kt
+++ shared/src/commonMain/kotlin/com/lunarlogic/aircasting/App.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
+import co.touchlab.kermit.Logger
 import com.lunarlogic.aircasting.bluetooth.AirBeamConnector
 import org.koin.compose.koinInject
 
@@ -17,6 +18,8 @@ fun App() {
   MaterialTheme {
     val connector = koinInject<AirBeamConnector>()
     val devices by connector.scan().collectAsState(initial = emptyList())
+    Logger.d(tag = "BLE") { "devices size: ${devices.size}" }
+    devices.forEach { Logger.d(tag = "BLE") { it.id.toString() } }
     LazyColumn { items(devices) { Text("${it.name} — ${it.device}") } }
   }
 }

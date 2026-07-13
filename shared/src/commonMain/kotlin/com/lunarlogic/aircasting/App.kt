dc9bc90647509b4b7a4fc5f12af19c0ae873fbf3
kgetman <kgetmanskii@gmail.com>
2026-07-13 15:43:11 +0200

    feat(AB BLE connection > prerequisites): gitignore md ai docs

--- shared/src/commonMain/kotlin/com/lunarlogic/aircasting/App.kt
+++ shared/src/commonMain/kotlin/com/lunarlogic/aircasting/App.kt
@@ -5,15 +5,11 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import co.touchlab.kermit.Logger
-import com.lunarlogic.aircasting.bluetooth.AirBeamConnector
 import com.lunarlogic.aircasting.ui.scan.ScanUiState
 import com.lunarlogic.aircasting.ui.scan.ScanViewModel
-import org.koin.compose.koinInject
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
